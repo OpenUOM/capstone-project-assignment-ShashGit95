0c71c1a19914906a295f37af7d9a944cd07e3f52
Shashika Janith Liyanage <shashika.janith.liyanage@gmail.com>
2023-09-19 14:12:57 +0530

    frontend change

--- frontend/src/app/components/teacher-table/teacher-table.component.ts
+++ frontend/src/app/components/teacher-table/teacher-table.component.ts
@@ -51,6 +51,16 @@ export class TeacherTableComponent implements OnInit {
     })
   }
 
+  getStudentData() {
+    this.selected = 'Students';
+    this.service.getStudentData().subscribe((response) => {
+      this.teacherData = response;
+    }, (error) => {
+      console.log('ERROR - ', error)
+    })
+  }
+
+
 
   search(value) {
     let foundItems = [];
